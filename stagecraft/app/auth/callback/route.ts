7dba001ab0d2695133d78e899b1c7f805a488e4f
BilguuteiTr <147648445+BilguuteiTr@users.noreply.github.com>
2026-02-07 14:46:50 -0600

    Remove comment on redirect URL in GET function
    Remove commented code about redirect URL handling.

--- stagecraft/app/auth/callback/route.ts
+++ stagecraft/app/auth/callback/route.ts
@@ -4,7 +4,6 @@ import { createClient } from '@/lib/supabase/server';
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get('code');
-  // if "next" is in param, use it as the redirect URL
   const next = searchParams.get('next') ?? '/marketplace';
 
   if (code) {
